12e2a2f1c430693e36c361c304bdafb27ac84cde
Robin Pierre RPS. Schreiner <rpschreiner@hms.halle.de>
2020-08-06 16:03:00 +0200

    Init commit

---
+++ source/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('postman-request')
+
+
+const forecast= (latitude, longitude, callback)=>{
+    const url= 'http://api.weatherstack.com/current?access_key=b2816eac8ce53ef0c8dcfea8142c240c&query=' + latitude + ',' + longitude
+
+    request({url,json:true},(error,response)=>{
+        if(error){
+            callback('Unable to connect to location services!', undefined)
+        } else if(response.body.error){
+            callback('unable to find location!',undefined)
+        } else {
+            callback(undefined, response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.'
+            )
+        }
+    })
+}
+module.exports = forecast
